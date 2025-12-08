a83eee5e2b726867555cbfe98e993711ff6e1e96
Francisco Espinosa <francisco.espinosaa87@gmail.com>
2025-12-08 16:30:47 -0600

    ver1.0

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const icons = document.querySelectorAll('.contact_icon svg');
 
 icons.forEach(icon => {
   icon.addEventListener('mouseover', () => {
-    icon.style.stroke = 'lightgray';
+    icon.style.stroke = '#1249d6';
 //     // icon.style.transform = 'scale(0.8)';
 //     // icon.style.filter = 'drop-shadow(0 0 6px #1249d6)';
   });
 
   icon.addEventListener('mouseout', () => {
-    icon.style.stroke = '#ffffff';
+    icon.style.stroke = '#000000';
 //     // icon.style.transform = 'scale(1)';
 //     // icon.style.filter = 'none';
   });
